5862b616f79bfe91868c37b0aea47195ca2b71dd
Colin Gage <colin.m.gage@gmail.com>
2017-07-11 21:20:08 -0700

    Completed project-2.js

--- src/project-2.js
+++ src/project-2.js
@@ -91,31 +91,41 @@ const isPrime = (num) => {
 
 const returnFirst = (arr) => {
   // return the first item from the array
+  return arr[0];
 };
 
 const returnLast = (arr) => {
   // return the last item of the array
+  return arr[arr.length - 1];
 };
 
 const getArrayLength = (arr) => {
   // return the length of the array
+  return arr.length;
 };
 
 const incrementByOne = (arr) => {
   // arr is an array of integers
   // increase each integer by one
   // return the array
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] += 1;
+  } return arr;
 };
 
 const addItemToArray = (arr, item) => {
   // add the item to the end of the array
   // return the array
+  arr.push(item);
+  return arr;
 };
 
 const addItemToFront = (arr, item) => {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 };
 
 const wordsToSentence = (words) => {
@@ -123,26 +133,47 @@ const wordsToSentence = (words) => {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  const sentence = words.join(' ');
+  return sentence;
 };
 
 const contains = (arr, item) => {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      return true;
+    }
+  } return false;
 };
 
 const addNumbers = (numbers) => {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  let sum = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    sum += numbers[i];
+  } return sum;
 };
 
 const averageTestScore = (testScores) => {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let sum = testScores[0];
+  for (let i = 1; i < testScores.length; i++) {
+    sum += testScores[i];
+  } return sum / testScores.length;
 };
 
 const largestNumber = (numbers) => {
   // numbers is an array of integers
   // return the largest integer
+  let largest = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (largest < numbers[i]) {
+      largest = numbers[i];
+    }
+  } return largest;
 };
 
 // Do not modify code below this line.
